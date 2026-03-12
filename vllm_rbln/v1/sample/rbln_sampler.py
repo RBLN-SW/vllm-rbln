b52e1e9428abfda1626279a035671643e082a2cf
rebel-yskim <rebel-yskim@github.com>
2026-03-12 16:04:49 +0900

    Remove compile_context + mark_static_address

--- vllm_rbln/v1/sample/rbln_sampler.py
+++ vllm_rbln/v1/sample/rbln_sampler.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # isort: off
-import inspect
 import torch
 import torch.nn as nn
 from vllm_rbln.logger import init_logger
@@ -133,7 +132,6 @@ class RBLNTopKTopPSampler(nn.Module):
         self,
         logprobs_mode: LogprobsMode = "raw_logprobs",
         seed: int = 42,
-        compile_context: rebel.CompileContext = None,
     ):
         # TODO(rbln): Merge more ops to rbln context.
         #       Currently, we only have softmax in rbln context.
@@ -145,16 +143,7 @@ class RBLNTopKTopPSampler(nn.Module):
         )
 
         rebel.manual_seed(seed)
-        options = {
-            "compile_context": compile_context
-            if compile_context
-            else (
-                rebel.CompileContext(use_global_ctx=True)
-                if "use_global_ctx"
-                in inspect.signature(rebel.CompileContext).parameters
-                else rebel.CompileContext()
-            )
-        }
+        options = {}
         if envs.VLLM_RBLN_COMPILE_STRICT_MODE:
             options["mode"] = "strict"
         self._compiled_rbln_topk_topp_sampler = torch.compile(
@@ -194,12 +183,11 @@ class RBLNSampler(VLLMSampler):
         self,
         logprobs_mode: LogprobsMode = "raw_logprobs",
         seed: int = 42,
-        compile_context: rebel.CompileContext = None,
     ):
         super().__init__()
         if logprobs_mode in ("raw_logprobs", "raw_logits"):
             self.topk_topp_sampler = RBLNTopKTopPSampler(
-                logprobs_mode=logprobs_mode, seed=seed, compile_context=compile_context
+                logprobs_mode=logprobs_mode, seed=seed,
             )
         else:
             logger.warning_once(
