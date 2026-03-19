49028403e7fa951eef1d79f18303e5de52cdb745
Huijong JEONG <huijong.jeong@squeezebits.com>
2026-03-19 02:44:05 +0000

    fix: return compilation time after warmup

--- vllm_rbln/v1/worker/rbln_worker.py
+++ vllm_rbln/v1/worker/rbln_worker.py
@@ -15,6 +15,7 @@
 
 import copy
 import os
+import time
 from types import NoneType
 from typing import TYPE_CHECKING
 
@@ -331,7 +332,8 @@ class RBLNWorker(WorkerBase):
         """Allocate RBLN KV cache with the specified kv_cache_config."""
         self.model_runner.initialize_kv_cache(kv_cache_config)
 
-    def compile_or_warm_up_model(self) -> None:
+    def compile_or_warm_up_model(self) -> float:
+        st = time.perf_counter()
         if self.parallel_config.data_parallel_size > 1:
             if envs.VLLM_RBLN_DP_IMPL == "padded_decode":
                 max_num_batched_tokens = self.scheduler_config.max_num_batched_tokens
@@ -353,35 +355,36 @@ class RBLNWorker(WorkerBase):
             or not envs.VLLM_RBLN_ENABLE_WARM_UP
         ):
             logger.warning("skipping compile_or_warm_up_model")
-            return
+        else:
+            try:
+                self.model_runner.warm_up_model()
 
-        try:
-            self.model_runner.warm_up_model()
+            except BackendCompilerFailed as e:
 
-        except BackendCompilerFailed as e:
+                def is_oom(exc):
+                    if isinstance(exc, RuntimeError):
+                        for arg in exc.args:
+                            if isinstance(arg, str) and (
+                                "SYS_ENOMEM: Out of memory" in arg
+                                or "SYS_EBUSY: Lack of device memory" in arg
+                            ):
+                                return True
+                    return False
 
-            def is_oom(exc):
-                if isinstance(exc, RuntimeError):
-                    for arg in exc.args:
-                        if isinstance(arg, str) and (
-                            "SYS_ENOMEM: Out of memory" in arg
-                            or "SYS_EBUSY: Lack of device memory" in arg
-                        ):
-                            return True
-                return False
+                if is_oom(e.inner_exception):
+                    raise RuntimeError(
+                        "Not enough memory for "
+                        f"{self.model_runner.kv_cache_config.num_blocks} "
+                        "blocks of KV cache. Try reducing the number of blocks "
+                        "by setting --num-gpu-blocks-override."
+                    ) from e
 
-            if is_oom(e.inner_exception):
-                raise RuntimeError(
-                    "Not enough memory for "
-                    f"{self.model_runner.kv_cache_config.num_blocks} "
-                    "blocks of KV cache. Try reducing the number of blocks "
-                    "by setting --num-gpu-blocks-override."
-                ) from e
-
-            raise
+                raise
 
         # after completing model warm up, enable RBLN performance tracker
         self.model_runner._enable_performance_tracker()
+
+        return time.perf_counter() - st
 
     def get_model(self) -> nn.Module:
         return self.model_runner.get_model()
