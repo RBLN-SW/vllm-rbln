bc044ea09165fcc53197c7187b87db3568b52061
rebel-thkim <rebel-thkim@github.com>
2026-03-19 11:26:36 +0900

    [WIP] batch, todo: rotary_emb

--- vllm_rbln/models/AXK1.py
+++ vllm_rbln/models/AXK1.py
@@ -47,32 +47,36 @@ def __AXK1_attention_forward(
     hidden_states: torch.Tensor,
     llama_4_scaling: torch.Tensor | None = None,
 ) -> torch.Tensor:
-    batch, _, _ = hidden_states.shape # 1, 128, 7168
+    batch, num_tokens, _ = hidden_states.shape # 1, 128, 7168
     if self.q_lora_rank is not None:
         q = self.q_a_proj(hidden_states)[0]
         q = self.q_a_layernorm(q) # torch.Size([1, 128, 1536])
-        q = self.q_b_proj(q)[0].view(-1, self.num_local_heads, self.qk_head_dim) # torch.size([128, 16, 192])
+        q = self.q_b_proj(q)[0].view(batch, num_tokens, self.num_local_heads, self.qk_head_dim) # torch.size([b, 128, 16, 192])
     else:
         q = self.q_proj(hidden_states)[0].view(
-            -1, self.num_local_heads, self.qk_head_dim
+            batch, num_tokens, self.num_local_heads, self.qk_head_dim
         )
     q_nope, q_pe = q.split([self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1) # torch.size([128, 16, 128]), torch.size([128, 16, 64])
     latent_cache = self.kv_a_proj_with_mqa(hidden_states)[0] # torch.size([1, 128, 576])
     
-    #rbln_fix: remove the first dimension of latent_cache
-    latent_cache = latent_cache.squeeze(0)
+    # #rbln_fix: remove the first dimension of latent_cache
+    # latent_cache = latent_cache.squeeze(0)
     
     kv_a, _ = latent_cache.split([self.kv_lora_rank, self.qk_rope_head_dim], dim=-1) # torch.Size([1, 128, 512])
-    latent_cache = latent_cache.unsqueeze(1) # torch.size([128, 1, 576])
+    latent_cache = latent_cache.unsqueeze(2) # torch.size([1, 128, 1, 576])
     kv_a = self.kv_a_layernorm(kv_a)
     kv = self.kv_b_proj(kv_a)[0] # torch.size([1,128, 4096])
-    kv = kv.view(-1, self.num_local_heads, self.qk_nope_head_dim + self.v_head_dim)
+    kv = kv.view(batch, -1, self.num_local_heads, self.qk_nope_head_dim + self.v_head_dim)
     k_nope, v = kv.split([self.qk_nope_head_dim, self.v_head_dim], dim=-1)
-    k_pe = latent_cache[:, :, self.kv_lora_rank :] # torch.size([128, 1, 64])
+    k_pe = latent_cache[..., self.kv_lora_rank :] # torch.size([128, 1, 64])
     q_pe, k_pe = self.rotary_emb(positions, q_pe, k_pe)
-        
-    q = torch.cat([q_nope, q_pe], dim=-1) # torch.size([128, 16, 192])
-    k = torch.cat([k_nope, k_pe.repeat(1, self.num_local_heads, 1)], dim=-1) # torch.size([128, 16, 192])
+
+
+# (*([FakeTensor(..., size=(2, 1, 16, 128), dtype=torch.bfloat16), FakeTensor(..., size=(2, 2, 16, 64), dtype=torch.bfloat16)],), **{'dim': -1}): got RuntimeError('Sizes of tensors must match except in dimension 3. Expected 1 in dimension 1 but got 2 for tensor number 1 in the list') 
+
+    #repeat(*(FakeTensor(..., size=(2, 2, 0, 64), dtype=torch.bfloat16), 1, 16, 1)
+    k = torch.cat([k_nope, k_pe.repeat(1, 1, self.num_local_heads, 1)], dim=-1) # torch.size([b, 128, 16, 192])
+    q = torch.cat([q_nope, q_pe], dim=-1) # torch.size([b, 128, 16, 192])
     
     # Apply llama 4 scaling if provided
     if llama_4_scaling is not None:
