7699953109e0d3bbe8515e50cb047f4fa8fcc372
rebel-eunji <eunji.lee@rebellions.ai>
2026-04-16 19:53:38 +0900

    idefics3

--- vllm_rbln/utils/optimum/multimodal/idefics3.py
+++ vllm_rbln/utils/optimum/multimodal/idefics3.py
@@ -19,4 +19,7 @@ from .common import get_language_model_config
 def get_param_idefics3(
     batch_size: int, max_model_len: int, block_size: int, tp_size: int
 ) -> dict:
-    return get_language_model_config(batch_size, max_model_len, block_size, tp_size)
+    language_model_config = get_language_model_config(
+        batch_size, max_model_len, block_size, tp_size
+    )
+    return {"text_model": language_model_config}
