643d489425c9969e806bbfdf41408ff628fa63f2
rebel-jinhwan <jinhwan.suk@rebellions.ai>
2026-04-23 14:47:51 +0900

    fix env var name

--- tests/torch_compile/e2e/v1/spec_decode/conftest.py
+++ tests/torch_compile/e2e/v1/spec_decode/conftest.py
@@ -17,7 +17,7 @@ import pytest
 
 @pytest.fixture(scope="module", autouse=True)
 def common_specdec_env(monkeypatch_module):
-    monkeypatch_module.setenv("VLLM_RBLN_USE_CUSTOM_KERNEL", "1")
+    monkeypatch_module.setenv("RBLN_USE_CUSTOM_KERNEL", "1")
     monkeypatch_module.setenv("VLLM_RBLN_COMPILE_STRICT_MODE", "1")
     monkeypatch_module.setenv("VLLM_DISABLE_COMPILE_CACHE", "1")
     monkeypatch_module.setenv("VLLM_RBLN_ENABLE_WARM_UP", "1")
