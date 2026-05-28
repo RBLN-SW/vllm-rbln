ebdcb2190b59d36c8d039825e9c0c10c025f809d
changjun.lee <changjun.lee@squeezebits.com>
2026-05-28 23:08:35 +0000

    refactor: streamline logging configuration

--- vllm_rbln/logger.py
+++ vllm_rbln/logger.py
@@ -13,50 +13,42 @@
 # limitations under the License.
 """Logging configuration for vLLM-RBLN Plugin."""
 
-import datetime
 import json
 import logging
-import os
-import sys
-from functools import lru_cache, partial
-from logging import Logger
 from logging.config import dictConfig
 from os import path
-from types import MethodType
-from typing import Any, cast
+from typing import Any
 
-import vllm.envs as envs
+from vllm import envs
+from vllm.logger import _DATE_FORMAT, _use_color, init_logger
 
-VLLM_RBLN_CONFIGURE_LOGGING = envs.VLLM_CONFIGURE_LOGGING
-VLLM_RBLN_LOGGING_CONFIG_PATH = envs.VLLM_LOGGING_CONFIG_PATH
-VLLM_RBLN_LOGGING_LEVEL = envs.VLLM_LOGGING_LEVEL
-VLLM_RBLN_LOGGING_PREFIX = envs.VLLM_LOGGING_PREFIX
-
-_FORMAT = (
-    f"[vllm-rbln] {VLLM_RBLN_LOGGING_PREFIX}%(levelname)s %(asctime)s "
-    "[%(filename)s:%(lineno)d] %(message)s"
-)
-_DATE_FORMAT = "%m-%d %H:%M:%S"
-
-DEFAULT_LOGGING_CONFIG = {
+_FORMAT = "[vllm-rbln] %(levelname)s %(asctime)s [%(fileinfo)s:%(lineno)d] %(message)s"
+DEFAULT_LOGGIN_CONFIG: dict[str, dict[str, Any] | Any] = {
     "formatters": {
-        "vllm": {
+        "vllm_rbln": {
             "class": "vllm.logging_utils.NewLineFormatter",
+            "datefmt": _DATE_FORMAT,
+            "format": _FORMAT,
+        },
+        "vllm_rbln_color": {
+            "class": "vllm.logging_utils.ColoredFormatter",
+            "datefmt": _DATE_FORMAT,
             "format": _FORMAT,
         },
     },
     "handlers": {
-        "vllm": {
+        "vllm_rbln": {
             "class": "logging.StreamHandler",
-            "formatter": "vllm",
-            "level": VLLM_RBLN_LOGGING_LEVEL,
-            "stream": "ext://sys.stdout",
+            # Choose formatter based on color setting.
+            "formatter": "vllm_rbln_color" if _use_color() else "vllm_rbln",
+            "level": envs.VLLM_LOGGING_LEVEL,
+            "stream": envs.VLLM_LOGGING_STREAM,
         },
     },
     "loggers": {
-        "vllm": {
-            "handlers": ["vllm"],
-            "level": "DEBUG",
+        "vllm_rbln": {
+            "handlers": ["vllm_rbln"],
+            "level": envs.VLLM_LOGGING_LEVEL,
             "propagate": False,
         },
     },
@@ -65,83 +57,41 @@ DEFAULT_LOGGING_CONFIG = {
 }
 
 
-@lru_cache
-def _print_debug_once(logger: Logger, msg: str) -> None:
-    # Set the stacklevel to 2 to print the original caller's line info
-    logger.debug(msg, stacklevel=2)
-
-
-@lru_cache
-def _print_info_once(logger: Logger, msg: str) -> None:
-    # Set the stacklevel to 2 to print the original caller's line info
-    logger.info(msg, stacklevel=2)
-
-
-@lru_cache
-def _print_warning_once(logger: Logger, msg: str) -> None:
-    # Set the stacklevel to 2 to print the original caller's line info
-    logger.warning(msg, stacklevel=2)
-
-
-class _VllmLogger(Logger):
-    """
-    Note:
-        This class is just to provide type information.
-        We actually patch the methods directly on the :class:`logging.Logger`
-        instance to avoid conflicting with other libraries such as
-        `intel_extension_for_pytorch.utils._logger`.
-    """
-
-    def debug_once(self, msg: str) -> None:
-        """
-        As :meth:`debug`, but subsequent calls with the same message
-        are silently dropped.
-        """
-        _print_debug_once(self, msg)
-
-    def info_once(self, msg: str) -> None:
-        """
-        As :meth:`info`, but subsequent calls with the same message
-        are silently dropped.
-        """
-        _print_info_once(self, msg)
-
-    def warning_once(self, msg: str) -> None:
-        """
-        As :meth:`warning`, but subsequent calls with the same message
-        are silently dropped.
-        """
-        _print_warning_once(self, msg)
-
-
 def _configure_vllm_root_logger() -> None:
-    logging_config = dict[str, Any]()
+    logging_config: dict[str, dict[str, Any] | Any] = {}
 
-    if not VLLM_RBLN_CONFIGURE_LOGGING and VLLM_RBLN_LOGGING_CONFIG_PATH:
+    if not envs.VLLM_CONFIGURE_LOGGING and envs.VLLM_LOGGING_CONFIG_PATH:
         raise RuntimeError(
-            "VLLM_RBLN_CONFIGURE_LOGGING evaluated to false, but "
-            "VLLM_RBLN_LOGGING_CONFIG_PATH was given."
-            "VLLM_RBLN_LOGGING_CONFIG_PATH "
-            "implies VLLM_RBLN_CONFIGURE_LOGGING. Please enable "
-            "VLLM_RBLN_CONFIGURE_LOGGING or "
-            "unset VLLM_RBLN_LOGGING_CONFIG_PATH."
+            "VLLM_CONFIGURE_LOGGING evaluated to false, but "
+            "VLLM_LOGGING_CONFIG_PATH was given. VLLM_LOGGING_CONFIG_PATH "
+            "implies VLLM_CONFIGURE_LOGGING. Please enable "
+            "VLLM_CONFIGURE_LOGGING or unset VLLM_LOGGING_CONFIG_PATH."
         )
 
-    if VLLM_RBLN_CONFIGURE_LOGGING:
-        logging_config = DEFAULT_LOGGING_CONFIG
+    if envs.VLLM_CONFIGURE_LOGGING:
+        logging_config = DEFAULT_LOGGIN_CONFIG
 
-    if VLLM_RBLN_LOGGING_CONFIG_PATH:
-        if not path.exists(VLLM_RBLN_LOGGING_CONFIG_PATH):
+        vllm_handler = logging_config["handlers"]["vllm_rbln"]
+        # Refresh these values in case env vars have changed.
+        vllm_handler["level"] = envs.VLLM_LOGGING_LEVEL
+        vllm_handler["stream"] = envs.VLLM_LOGGING_STREAM
+        vllm_handler["formatter"] = "vllm_rbln_color" if _use_color() else "vllm_rbln"
+
+        vllm_loggers = logging_config["loggers"]["vllm_rbln"]
+        vllm_loggers["level"] = envs.VLLM_LOGGING_LEVEL
+
+    if envs.VLLM_LOGGING_CONFIG_PATH:
+        if not path.exists(envs.VLLM_LOGGING_CONFIG_PATH):
             raise RuntimeError(
                 "Could not load logging config. File does not exist: %s",
-                VLLM_RBLN_LOGGING_CONFIG_PATH,
+                envs.VLLM_LOGGING_CONFIG_PATH,
             )
-        with open(VLLM_RBLN_LOGGING_CONFIG_PATH, encoding="utf-8") as file:
+        with open(envs.VLLM_LOGGING_CONFIG_PATH, encoding="utf-8") as file:
             custom_config = json.loads(file.read())
 
         if not isinstance(custom_config, dict):
             raise ValueError(
-                "Invalid logging config. Expected Dict, got %s.",
+                "Invalid logging config. Expected dict, got %s.",
                 type(custom_config).__name__,
             )
         logging_config = custom_config
@@ -155,94 +105,14 @@ def _configure_vllm_root_logger() -> None:
         dictConfig(logging_config)
 
 
-def init_logger(name: str) -> _VllmLogger:
-    """The main purpose of this function is to ensure that loggers are
-    retrieved in such a way that we can be sure the root vllm logger has
-    already been configured."""
-
-    logger = logging.getLogger(f"vllm.{name}")
-
-    methods_to_patch = {
-        "debug_once": _print_debug_once,
-        "info_once": _print_info_once,
-        "warning_once": _print_warning_once,
-    }
-
-    for method_name, method in methods_to_patch.items():
-        setattr(logger, method_name, MethodType(method, logger))
-
-    return cast(_VllmLogger, logger)
-
-
 # The root logger is initialized when the module is imported.
 # This is thread-safe as the module is only imported once,
 # guaranteed by the Python GIL.
 _configure_vllm_root_logger()
 
+# Transformers uses httpx to access the Hugging Face Hub. httpx is quite verbose,
+# so we set its logging level to WARNING when vLLM's logging level is INFO.
+if envs.VLLM_LOGGING_LEVEL == "INFO":
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
 logger = init_logger(__name__)
-
-
-def _trace_calls(log_path, root_dir, frame, event, arg=None):
-    if event in ["call", "return"]:
-        # Extract the filename, line number, function name, and the code object
-        filename = frame.f_code.co_filename
-        lineno = frame.f_lineno
-        func_name = frame.f_code.co_name
-        if not filename.startswith(root_dir):
-            # only log the functions in the vllm root_dir
-            return
-        # Log every function call or return
-        try:
-            last_frame = frame.f_back
-            if last_frame is not None:
-                last_filename = last_frame.f_code.co_filename
-                last_lineno = last_frame.f_lineno
-                last_func_name = last_frame.f_code.co_name
-            else:
-                # initial frame
-                last_filename = ""
-                last_lineno = 0
-                last_func_name = ""
-            with open(log_path, "a") as f:
-                ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                if event == "call":
-                    f.write(
-                        f"{ts} Call to"
-                        f" {func_name} in {filename}:{lineno}jiwoo"
-                        f" from {last_func_name} in {last_filename}:"
-                        f"{last_lineno}\n"
-                    )
-                else:
-                    f.write(
-                        f"{ts} Return from"
-                        f" {func_name} in {filename}:{lineno}jiwoo"
-                        f" to {last_func_name} in {last_filename}:"
-                        f"{last_lineno}\n"
-                    )
-        except NameError:
-            # modules are deleted during shutdown
-            pass
-    return partial(_trace_calls, log_path, root_dir)
-
-
-def enable_trace_function_call(log_file_path: str, root_dir: str | None = None):
-    """
-    Enable tracing of every function call in code under `root_dir`.
-    This is useful for debugging hangs or crashes.
-    `log_file_path` is the path to the log file.
-    `root_dir` is the root directory of the code to trace. If None, it is the
-    vllm root directory.
-
-    Note that this call is thread-level, any threads calling this function
-    will have the trace enabled. Other threads will not be affected.
-    """
-    logger.warning(
-        "VLLM_RBLN_TRACE_FUNCTION is enabled. It will record every"
-        " function executed by Python. This will slow down the code. It "
-        "is suggested to be used for debugging hang or crashes only."
-    )
-    logger.info("Trace frame log is saved to %s", log_file_path)
-    if root_dir is None:
-        # by default, this is the vllm root directory
-        root_dir = os.path.dirname(os.path.dirname(__file__))
-    sys.settrace(partial(_trace_calls, log_file_path, root_dir))
