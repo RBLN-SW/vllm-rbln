11c8120c828763d8dc50f2eb93e8e6ccad9140db
rebel-eunji <eunji.lee@rebellions.ai>
2026-06-04 21:28:53 +0900

    comment

--- vllm_rbln/utils/optimum/registry.py
+++ vllm_rbln/utils/optimum/registry.py
@@ -121,13 +121,14 @@ def is_arch_supported(
 ) -> bool:
     architectures = getattr(config, "architectures", [])
     import vllm.model_executor.models as me_models
+
     registry = me_models.ModelRegistry
-    # if architectures is not registry.get_supported_archs():
-    #     raise ValueError(
-    #         f"Model architectures {architectures} are not supported on upstream vLLM "
-    #         f"for now. Supported architectures: "
-    #         f"{registry.get_supported_archs()}"
-    #     )
+    if architectures is not registry.get_supported_archs():
+        raise ValueError(
+            f"Model architectures {architectures} are not supported on upstream vLLM "
+            f"for now. Supported architectures: "
+            f"{registry.get_supported_archs()}"
+        )
     return any(
         arch in _RBLN_SUPPORTED_MODELS and arch in model_set for arch in architectures
     )
