02033d9e83a510386a87d6b1839c8a2eccf1ebed
rebel-eunji <eunji.lee@rebellions.ai>
2026-06-25 21:40:38 +0900

    corefix

--- vllm_rbln/model_executor/models/optimum/optimum_attention/optimum_attention_strategy.py
+++ vllm_rbln/model_executor/models/optimum/optimum_attention/optimum_attention_strategy.py
@@ -303,7 +303,7 @@ class HybridAttentionImageStrategy(
         padded_local_block_table_ids = self.pad_to_2d(
             local_block_table_ids, decoder_batch_size, 1, pad_value, torch.int16
         )
-        # padded_pad_len = self.pad_to_2d(pad_lens, decoder_batch_size, 1, 0)
+        padded_pad_len = self.pad_to_2d(pad_lens, decoder_batch_size, 1, 0)
         padded_cache_positions = self.pad_to_2d(
             cache_positions, decoder_batch_size, 1, 0
         )
@@ -312,6 +312,7 @@ class HybridAttentionImageStrategy(
         )
         # FIXME remove the logic
         position_ids = padded_cache_positions
+        padded_cache_positions = position_ids + padded_pad_len
 
         return (
             padded_local_block_table_ids,
