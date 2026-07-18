aaedb8110f93ab1537079ec021e9999228181ce3
rebel-eunji <eunji.lee@rebellions.ai>
2026-07-18 20:26:01 +0900

    dtype casting2

--- vllm_rbln/platform.py
+++ vllm_rbln/platform.py
@@ -297,7 +297,6 @@ class RblnPlatform(Platform):
                 model_config.disable_cascade_attn = True
 
         else:
-            # # NOTE(eunji.lee):
             if forces_fp32_dtype(vllm_config.model_config):
                 model_config.dtype = torch.float32
 
