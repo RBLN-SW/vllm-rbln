3368d5411af426dd7a2934219a3452bcf31e8a26
kblee <kblee@rebellions.ai>
2026-08-25 19:19:01 +0900

    review to remove bind kv cache

--- vllm_rbln/v1/worker/rbln_model_runner.py
+++ vllm_rbln/v1/worker/rbln_model_runner.py
@@ -98,7 +98,6 @@ from vllm.v1.worker.utils import (
     KVCacheGroupSpec,
     KVCacheSpec,
     add_kv_sharing_layers_to_kv_cache_groups,
-    bind_kv_cache,
 )
 
 from vllm_rbln import envs
@@ -2647,14 +2646,11 @@ class RBLNModelRunner(KVConnectorModelRunnerMixin):
             kv_cache_view_infos,
             num_attn_module,
         )
-        bind_kv_cache(
-            kv_caches,
-            self.compilation_config.static_forward_context,
-            self.kv_caches,
-            num_attn_module,
-        )
         self.kv_cache_names = get_kv_cache_names(kv_caches, num_attn_module)
-        assert len(self.kv_cache_names) == len(self.kv_caches)
+        self.kv_caches = [kv_caches[name] for name in self.kv_cache_names]
+        forward_context = self.compilation_config.static_forward_context
+        for layer_name, kv_cache in kv_caches.items():
+            forward_context[layer_name].kv_cache = kv_cache
 
         if (
             not USE_DEVICE_TENSOR
